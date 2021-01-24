4fc5a1228536c4cae32ba5e128f3aac17bd398d9
Joachim Valente <joachim.valente@gmail.com>
2021-01-24 14:26:14 -0800

    static

--- lib/side_header_list_view.dart
+++ lib/side_header_list_view.dart
@@ -36,66 +36,74 @@ class SideHeaderListView extends StatefulWidget {
 }
 
 class _SideHeaderListViewState extends State<SideHeaderListView> {
-  int currentPosition = 0;
   final _controller = ScrollController();
+  static double _controllerOffset = 0.0;
 
   @override
   void initState() {
     super.initState();
+    print('in initState');
     // Reposition side view each time the main list view is scrolled
     _controller.addListener(_reposition);
     // In case the initial offset is not 0, we reposition the side view to the
     // correct offset after first build
     // TODO: this produces a flickering effect -- we should try to position it
     // correctly before the first build
-    WidgetsBinding.instance.addPostFrameCallback((_) => _reposition());
+    // WidgetsBinding.instance.addPostFrameCallback((_) => _reposition());
   }
 
   @override
   void dispose() {
+    print('in dispose');
     _controller.removeListener(_reposition);
     super.dispose();
   }
 
   void _reposition() {
-    setState(() =>
-        currentPosition = (_controller.offset / widget.itemExtent).floor());
+    print('in reposition');
+    setState(() => _controllerOffset = _controller.offset);
   }
 
   @override
-  Widget build(BuildContext context) => Stack(
-        children: <Widget>[
-          Positioned(
-            child: Opacity(
-              opacity: _shouldShowHeader(currentPosition) ? 0.0 : 1.0,
-              child: widget.headerBuilder(
-                  context, currentPosition >= 0 ? currentPosition : 0),
-            ),
-            top: 0.0 + (widget.padding?.top ?? 0),
-            left: 0.0 + (widget.padding?.left ?? 0),
+  Widget build(BuildContext context) {
+    print('in build');
+    final currentPosition = (_controllerOffset / widget.itemExtent).floor();
+    return Stack(
+      children: <Widget>[
+        Positioned(
+          child: Opacity(
+            opacity:
+                _shouldShowHeader(currentPosition, currentPosition) ? 0.0 : 1.0,
+            child: widget.headerBuilder(
+                context, currentPosition >= 0 ? currentPosition : 0),
           ),
-          ListView.builder(
-            padding: widget.padding,
-            itemCount: widget.itemCount,
-            itemExtent: widget.itemExtent,
-            controller: _controller,
-            itemBuilder: (BuildContext context, int index) => Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                FittedBox(
-                  child: Opacity(
-                    opacity: _shouldShowHeader(index) ? 1.0 : 0.0,
-                    child: widget.headerBuilder(context, index),
-                  ),
+          top: 0.0 + (widget.padding?.top ?? 0),
+          left: 0.0 + (widget.padding?.left ?? 0),
+        ),
+        ListView.builder(
+          padding: widget.padding,
+          itemCount: widget.itemCount,
+          itemExtent: widget.itemExtent,
+          controller: _controller,
+          itemBuilder: (BuildContext context, int index) => Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              FittedBox(
+                child: Opacity(
+                  opacity:
+                      _shouldShowHeader(index, currentPosition) ? 1.0 : 0.0,
+                  child: widget.headerBuilder(context, index),
                 ),
-                Expanded(child: widget.itemBuilder(context, index))
-              ],
-            ),
+              ),
+              Expanded(child: widget.itemBuilder(context, index))
+            ],
           ),
-        ],
-      );
+        ),
+      ],
+    );
+  }
 
-  bool _shouldShowHeader(int position) =>
+  bool _shouldShowHeader(int position, int currentPosition) =>
       (position < 0) ||
       (position == 0 && currentPosition < 0) ||
       (position != 0 &&
